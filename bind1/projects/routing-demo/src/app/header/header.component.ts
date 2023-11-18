96cc4f9552feae397565735063b97d1ff1293e46
af2al <afzal.hussain@sailssoftware.com>
2023-11-18 17:32:23 +0530

    SnakeGameApplication

--- bind1/projects/routing-demo/src/app/header/header.component.ts
+++ bind1/projects/routing-demo/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +9,22 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HeaderComponent {
   name = '';
+
+  simpleObservable = new Observable((observer) => {
+    observer.next("hello");
+    observer.next("world");
+    observer.complete();
+      
+
+  })
+
+
   constructor(private pr : ActivatedRoute) {
     // this.pr.queryParams.subscribe((p)=>console.log(p))
     this.pr.queryParams.subscribe((p)=> {
     this.name=p['name'];
+
+    this.simpleObservable.subscribe((s=>console.log(s)))
   })
 }
 }
