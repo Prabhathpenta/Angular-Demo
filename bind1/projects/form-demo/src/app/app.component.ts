d8a9621687ecbeb95d6b257d0f11ee69662ff82c
af2al <afzal.hussain@sailssoftware.com>
2023-11-22 11:24:38 +0530

    freeDemo

--- bind1/projects/form-demo/src/app/app.component.ts
+++ bind1/projects/form-demo/src/app/app.component.ts
@@ -8,7 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'formDemo';
 
-  display(f:any, s:any) {
-    console.log(f,s);
+  display(f:any,a:any, b:any, c:any) {
+    console.log(f,a,b,c);
+    console.log(c.value);
+
+    f.setValue ({
+      username:"prabha",
+      pswrd:123456,
+      email:"prty23@sdf.com"
+    })
+}
+  submit() {
+
   }
+
 }
--- bind1/projects/form-demo/src/app/app.module.ts
+++ bind1/projects/form-demo/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material/material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
